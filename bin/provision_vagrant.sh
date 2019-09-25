4982254ed273427d5f8c41b5cbcf78d3fa2dcfb0
Moe's <moes-thai-macbook-pro@ninezh.cc>
2019-09-25 13:35:09 +0200

    - Add static dns of 192.168.50.4... because mac is stupid

--- bin/provision_vagrant.sh
+++ bin/provision_vagrant.sh
@@ -3,3 +3,4 @@
 #sudo su vagrant -c 'cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys'
 chmod +x /home/vagrant/add_sshkey.sh
 sudo su vagrant -c 'cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys'
+echo "nameserver 192.168.50.4" > /etc/resolv.conf
