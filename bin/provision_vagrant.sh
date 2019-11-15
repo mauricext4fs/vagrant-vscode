9b84a3b62d4009d0fc6ad0cfa284f811828fdb33
Maurice Courtois <mcourtois@vlad-Baar>
2019-11-15 01:17:42 +0100

    - Add postgres - Add golang - Add dredd - Add nodejs - Add redis - Add nginx

--- bin/provision_vagrant.sh
+++ bin/provision_vagrant.sh
@@ -3,4 +3,39 @@
 #sudo su vagrant -c 'cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys'
 chmod +x /home/vagrant/add_sshkey.sh
 sudo su vagrant -c 'cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys'
+# Install PostgreSQL
+sudo yum update && yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo yum install -y postgresql12 postgresql12-server
+sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
+sudo systemctl enable postgresql-12
+sudo systemctl start postgresql-12
 echo "nameserver 192.168.50.4" > /etc/resolv.conf
+
+# Install GoLang
+wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.13.4.linux-amd64.tar.gz
+rm go1.13.4.linux-amd64.tar.gz
+
+# Install Redis
+sudo yum install redis -y
+sudo systemctl enable redis
+echo "bind 0.0.0.0" > /etc/redis.conf
+echo "protected-mode no" >> /etc/redis.conf
+sudo systemctl enable redis
+sudo systemctl start redis
+
+# Install nodejs, yarn and dredd
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+sudo yum install -y  yarn
+sudo yarn global add dredd
+
+# Add bind volume for Nginx
+echo "/home/vagrant/cig/root/etc/nginx/public_html   /var/www/html     none    bind                      0       0" >> /etc/fstab
+sudo yum install -y nginx
+cd /etc/nginx/conf.d/ && sudo rm default.conf && sudo ln -s /home/vagrant/cig/root/etc/nginx/vagrant-cig-local.ninezh.cc.conf default.conf
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+
